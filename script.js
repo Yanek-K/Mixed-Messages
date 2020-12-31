f274eb5c6683ee171c5c0f9faab20709f418f0c9
Yanek K <yanek.dev@gmail.com>
2020-12-30 20:51:15 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let personality = ["Adventurous", "Helpful", "Affable", "Humble", "Capable", 
+    "Imaginative", "Cultured", "Optimistic", "Persistent", "Sociable"]
+
+let hobbies = ["Archery", "Backpacking", "Beekeeping", "Blacksmithing", 
+    "Bodybuilding", "Fishing", "Flying", "Foraging", "Hunting", "Kayaking"]
+
+let languages = ["Mandarin-Chinese", "Hindi", "Spanish", "Standard-Arabic", "Bengali",
+    "Russian", "Portuguese", "Indonesian", "Urdu", "Japanese"]
+
+let random = Math.floor(Math.random() * 10)
+console.log(random)
