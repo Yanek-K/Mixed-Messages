82ac93bc22db32e481c93fd079d5fe057aeff679
Yanek K <yanek.dev@gmail.com>
2020-12-30 21:23:05 -0500

    Added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Info for new personality */
 let personality = ["Adventurous", "Helpful", "Affable", "Humble", "Capable", 
     "Imaginative", "Cultured", "Optimistic", "Persistent", "Sociable"]
 
@@ -7,15 +8,17 @@ let hobbies = ["Archery", "Backpacking", "Beekeeping", "Blacksmithing",
 let languages = ["Mandarin-Chinese", "Hindi", "Spanish", "Standard-Arabic", "Bengali",
     "Russian", "Portuguese", "Indonesian", "Urdu", "Japanese"]
 
+/* Logic */    
+
 let random = Math.floor(Math.random())
 
 let randomLang = languages[ Math.floor(Math.random() * languages.length)];
 let randomHobbie = hobbies[Math.floor(Math.random() * hobbies.length)];
 let randomPersonality = personality[Math.floor(Math.random() * personality.length)];
 
-// console.log(randomLang);
-// console.log(randomHobbie);
-// console.log(randomPersonality);
+
+/* Final string to submit to user */
+
 
 console.log(`Welcome to your new life.
 
