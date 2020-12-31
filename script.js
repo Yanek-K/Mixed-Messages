ecdb0b5df98cc05007ad15ebd909602dc08ab0b5
Yanek K <yanek.dev@gmail.com>
2020-12-30 21:19:53 -0500

    Completed logic

--- script.js
+++ script.js
@@ -7,5 +7,29 @@ let hobbies = ["Archery", "Backpacking", "Beekeeping", "Blacksmithing",
 let languages = ["Mandarin-Chinese", "Hindi", "Spanish", "Standard-Arabic", "Bengali",
     "Russian", "Portuguese", "Indonesian", "Urdu", "Japanese"]
 
-let random = Math.floor(Math.random() * 10)
-console.log(random)
+let random = Math.floor(Math.random())
+
+let randomLang = languages[ Math.floor(Math.random() * languages.length)];
+let randomHobbie = hobbies[Math.floor(Math.random() * hobbies.length)];
+let randomPersonality = personality[Math.floor(Math.random() * personality.length)];
+
+// console.log(randomLang);
+// console.log(randomHobbie);
+// console.log(randomPersonality);
+
+console.log(`Welcome to your new life.
+
+For your future, our program has decided:
+
+Your new language will be:
+${randomLang}.
+How does that sound?
+
+Your new favorite hobby is:
+${randomHobbie}. 
+
+The personality trait you must embody is:
+${randomPersonality}. 
+
+
+Get to it!!`)
